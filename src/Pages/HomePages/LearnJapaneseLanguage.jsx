5440065b2535813f45db211e904929f88bf1ede2
teamdigital39 <teamdigital39@gmail.com>
2026-09-10 21:35:59 +0530

    Update LearnJapaneseLanguage.jsx

--- src/Pages/HomePages/LearnJapaneseLanguage.jsx
+++ src/Pages/HomePages/LearnJapaneseLanguage.jsx
@@ -495,7 +495,7 @@ export default function LangmaJapaneseCourse() {
       travel: "Interested in Travel & Culture Program"
     };
     try {
-      const res = await fetch(`${API_BASE}/apply-submit`, {
+      const res = await fetch(`${API_BASE}/api/apply-submit`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
