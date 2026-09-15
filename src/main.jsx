b7a97355b549cac065558cd56e4b467eaabc0a29
teamdigital39 <teamdigital39@gmail.com>
2026-09-15 14:20:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import CanonicalUrl from "./CanonicalUrl";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <CanonicalUrl />
       <App />
     </BrowserRouter>
   </React.StrictMode>
