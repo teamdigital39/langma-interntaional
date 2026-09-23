4167befb0211d13d070890f3f483bb82e85a7236
teamdigital39 <teamdigital39@gmail.com>
2026-09-23 19:01:34 +0530

    Deploy validated Langma local updates: src/standaloneLandingRoutes.js

---
+++ src/standaloneLandingRoutes.js
@@ -0,0 +1,12 @@
+export const STANDALONE_LANDING_ROUTES = [
+  "/learn-german-language",
+  "/learn-korean-language",
+  "/learn-japanese-language",
+  "/translation-services",
+  "/learn-french-language",
+  "/learn-chinese-language",
+];
+
+export const GTM_ID = "GTM-PJSJB68W";
+export const GOOGLE_ADS_ID = "AW-1001957978";
+export const GOOGLE_ANALYTICS_ID = "G-L5WSZMPZ4Y";
