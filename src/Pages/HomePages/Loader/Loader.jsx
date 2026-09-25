6099cad861d79777d8b855bcdab069bfb1576498
teamdigital39 <teamdigital39@gmail.com>
2026-09-25 13:37:31 +0530

    Improve shared image dimensions and alt text

--- src/Pages/HomePages/Loader/Loader.jsx
+++ src/Pages/HomePages/Loader/Loader.jsx
@@ -5,6 +5,8 @@ const Loader = () => {
     <div className="fixed inset-0 flex items-center justify-center bg-white z-[9999]">
       <img
         src="/images/Logo.png"
+        width={1473}
+        height={512}
         alt="Langma International"
         className="w-[200px] sm:w-[260px] md:w-[300px] h-auto object-contain animate-pulse"
       />
